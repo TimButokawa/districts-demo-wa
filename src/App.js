c27d9be7f3f7a8c733ec0ff662c47bfa53e6c57b
Tim Butorac <tim@timbutorac.com>
2017-05-22 09:22:42 -0700

    add leaflets map to home page and remove map page from routes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Header from './components/header/Header.component';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Districts from './routes/districts/Districts.container';
-import DistrictsMap from './routes/map/DistrictsMap.container';
 import Home from './routes/home/Home.container';
 
 class App extends Component {
@@ -16,7 +15,6 @@ class App extends Component {
             <div style={{marginTop: '64px', flexGrow: 1, padding: '24px'}}>
               <Route exact path='/' component={Home}/>
               <Route path='/districts' component={Districts}/>
-              <Route path='/map' component={DistrictsMap}/>
             </div>
           </div>
         </MuiThemeProvider>
--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,6 +1,13 @@
 import React, {Component} from 'react';
 import Layout from 'material-ui/Layout';
 import {Link} from 'react-router-dom';
+import L from 'leaflet';
+
+const styles = {
+  map: {
+    height: '500px'
+  }
+};
 
 class Home extends Component {
   componentWillMount() {
@@ -10,13 +17,24 @@ class Home extends Component {
     }
   }
 
+  componentDidMount() {
+    const accessToken = 'pk.eyJ1IjoidGJ1dG9yIiwiYSI6ImNqMzBhYzgyeDAwOGczNW9rbDNhNDdhcGYifQ.q7B9wNdpPz_I6fIKmS2ZFw';
+    const myMap = L.map('mapId', {
+        center: [47.3816144, -119.5585346],
+        zoom: 7
+    });
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
+      accessToken: accessToken
+    }).addTo(myMap);
+  }
+
   render() {
     return (
       <Layout container gutter={16}>
         <Layout item xs={12}>
-        <img src="https://unsplash.it/1920/1080?image=2" alt="Contemplative Reptile" style={{maxWidth: '100%', display: 'block'}}/>
-        <Link to="/districts">Districts Cards</Link>
-        <br/>
+          <div style={styles.map} id="mapId"></div>
+          <Link to="/districts">Districts Cards</Link>
         </Layout>
       </Layout>
     );
