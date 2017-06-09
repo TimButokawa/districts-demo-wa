0dcc989c62c8da8a62b0e39df88f548d7627fe01
Tim Butorac <tim@timbutorac.com>
2017-06-08 23:22:51 -0700

    fix issue with devtools breaking the build by composing with redux dev tools extension

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,14 +1,14 @@
-import {createStore, applyMiddleware, compose} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
+import {composeWithDevTools} from 'redux-devtools-extension';
 
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    compose(
-      applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeWithDevTools(
+      applyMiddleware(thunk)
     )
   );
 }
