c978991830be8b893c33749cf93feff46bf3e22b
Tim Butorac <tim@timbutorac.com>
2017-06-14 09:51:23 -0700

    move map to its own component - update render loading conditions

--- src/routes/map/Map.js
+++ src/routes/map/Map.js
@@ -3,19 +3,9 @@ import PropTypes from 'prop-types';
 import Layout from 'material-ui/Layout';
 import Button from 'material-ui/Button';
 import {withRouter} from 'react-router';
-import L from 'leaflet';
-import {mapConfig} from '../../utils/map.utils';
-import Loader from '../../components/loader/Loader.component';
 
-const styles = {
-  map: {
-    height: '600px'
-  },
-  borders: {
-    color: '#4d4d4d',
-    weight: 1
-  }
-};
+import Loader from '../../components/loader/Loader.component';
+import DistrictMap from '../../components/district-map/DistrictMap.component';
 
 class Map extends Component {
   constructor(props) {
@@ -35,50 +25,28 @@ class Map extends Component {
     }
   }
 
-  componentDidMount() {
-    const districtMap = L.map(mapConfig.id, mapConfig.default);
-    this.setState({map: districtMap});
-
-    L.tileLayer(mapConfig.tileUrl, {accessToken: mapConfig.accessToken}).addTo(districtMap);
-  }
-
   render() {
     const {districtsGeo} = this.props;
-    const loader = districtsGeo.length ? null : <Loader/>;
-
-    if (districtsGeo.length && this.state) {
-      L.geoJSON(districtsGeo, {
-        onEachFeature: _onEachFeature,
-        style: styles.borders,
-      }).addTo(this.state.map);
-    }
 
     return (
       <Layout container gutter={16}>
-        <Layout item xs={12}>
-          {loader}
-          <div style={styles.map} id="mapId"></div>
-        </Layout>
-        <Layout container align="center" justify="center">
-          <Layout item>
-            <Button onClick={() => this.handleNavigation('/')}>Back</Button>
-          </Layout>
-        </Layout>
+        {districtsGeo.length ?
+          <Layout container>
+            <Layout item xs={12}>
+              <DistrictMap districtsGeo={districtsGeo}/>
+            </Layout>
+            <Layout container align="center" justify="center">
+              <Layout item>
+                <Button onClick={() => this.handleNavigation('/')}>Back</Button>
+              </Layout>
+            </Layout>
+          </Layout> : <Loader/>}
       </Layout>
     );
   }
 }
 
-function _onEachFeature(feature, layer) {
-  const title = feature.properties.name;
-  const template = [
-    '<div>',
-      '<h3>' + title + '</h3>',
-    '</div>'
-  ].join('');
-  const popup = L.popup().setContent(template);
-  layer.bindPopup(popup);
-}
+
 
 Map.propTypes = {
   districtAction: PropTypes.object,
