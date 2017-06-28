f178f817f3c93b66da0c6856af22f64eee8b51d8
Tim Butorac <tim@timbutorac.com>
2017-06-28 12:37:50 -0700

    revise map to use react-leaflet components for easier lifecycle rendering - remove set content from feature popup

--- src/components/district-map/DistrictMap.component.js
+++ src/components/district-map/DistrictMap.component.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import L from 'leaflet';
+import {Map, TileLayer, GeoJSON} from 'react-leaflet';
 import {mapConfig} from '../../utils/map.utils';
 
 const styles = {
@@ -14,23 +14,30 @@ const styles = {
 };
 
 class DistrictMap extends Component {
-  componentDidMount() {
-    const districtMap = L.map(mapConfig.id, mapConfig.default);
-    this.setState({map: districtMap});
-
-    L.tileLayer(mapConfig.tileUrl, {accessToken: mapConfig.accessToken}).addTo(districtMap);
-  }
 
   render() {
     const {districtsGeo} = this.props;
-    if (districtsGeo.length && this.state) {
-      L.geoJSON(districtsGeo, {
-        onEachFeature: _onEachFeature,
-        style: styles.borders,
-      }).addTo(this.state.map);
-    }
 
-    return <div style={styles.map} id="mapId"></div>;
+    return (
+      <div>
+        <Map
+          center={mapConfig.default.center}
+          zoom={mapConfig.default.zoom}
+          minZoom={mapConfig.default.minZoom}
+          maxZoom={mapConfig.default.maxZoom}
+          scrollWheelZoom={mapConfig.default.scrollWheelZoom}
+          style={styles.map}>
+          <TileLayer
+            url={mapConfig.tileUrl}
+            accessToken={mapConfig.accessToken}/>
+          <GeoJSON
+              data={districtsGeo}
+              style={styles.borders}
+              onEachFeature={_onEachFeature}/>
+        </Map>
+        <div id="mapId"></div>
+      </div>
+    );
   }
 }
 
@@ -41,8 +48,7 @@ function _onEachFeature(feature, layer) {
       '<h3>' + title + '</h3>',
     '</div>'
   ].join('');
-  const popup = L.popup().setContent(template);
-  layer.bindPopup(popup);
+  layer.bindPopup(template);
 }
 
 DistrictMap.propTypes = {
