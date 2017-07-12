3771e11600c6ce93ec69ca9711ab632ba76d8aa7
Tim Butorac <tim@timbutorac.com>
2017-07-11 18:07:36 -0700

    update home container to reflect demographics state revisions

--- src/routes/home/Home.container.js
+++ src/routes/home/Home.container.js
@@ -1,25 +1,33 @@
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {getDistrictsGeo, getDistrictGeo} from '../../selectors/districts-geo';
-import {getDisplayFavorites} from '../../selectors/interface';
+import {getDistrictsDemo, getDistrictDemo} from '../../selectors/districts';
+import {getDisplayFavorites, getChartType} from '../../selectors/interface';
 import * as DistrictGeoActions from '../../actions/districts-geo';
+import * as DemographicsActions from '../../actions/districts';
 import * as FavoritesActions from '../../actions/favorites';
+import * as InterfaceActions from '../../actions/interface';
 
 import Home from './Home';
 
 const mapStateToProps = state => {
   return {
     districts: getDistrictsGeo(state),
+    demographics: getDistrictsDemo(state),
     selectedDistrict: getDistrictGeo(state),
+    selectedDemographics: getDistrictDemo(state),
     displayFavorites: getDisplayFavorites(state),
-    favorites: state.favorites.data
+    favorites: state.favorites.data,
+    chartType: getChartType(state)
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
     districtGeoAction: bindActionCreators(DistrictGeoActions, dispatch),
-    favoritesAction: bindActionCreators(FavoritesActions, dispatch)
+    demographicsAction: bindActionCreators(DemographicsActions, dispatch),
+    favoritesAction: bindActionCreators(FavoritesActions, dispatch),
+    interfaceAction: bindActionCreators(InterfaceActions, dispatch)
   };
 }
 
--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import DistrictMap from '../../components/district-map/DistrictMap.component';
 import DistrictList from '../../components/district-list/DistrictList.component';
+import DistrictCard from '../../components/district-card/DistrictCard.component';
+import PopulationChart from '../../components/population-chart/PopulationChart.component';
 import Loader from '../../components/loader/Loader.component';
 import Paper from 'material-ui/Paper';
 
@@ -16,16 +18,28 @@ const styles = {
   },
   mainContent: {
     flexGrow: '1',
-    padding: '16px'
+    padding: '16px',
+    position: 'relative'
+  },
+  dialog: {
+    position: 'absolute',
+    top: 16,
+    right: 16,
+    zIndex: 1001,
+    flexDirection: 'column'
   }
 };
 
 class Home extends Component {
 
   componentWillMount() {
-    const {districts} = this.props;
+    const {districts, demographics} = this.props;
     if (!districts.length) {
       this.props.districtGeoAction.requestDistrictsGeo();
+    }
+
+    if (!demographics.length) {
+      this.props.demographicsAction.requestDemographics();
     }
   }
 
@@ -33,10 +47,14 @@ class Home extends Component {
     const {
       districts,
       selectedDistrict,
+      selectedDemographics,
       favorites,
       displayFavorites,
       favoritesAction,
-      districtGeoAction
+      districtGeoAction,
+      demographicsAction,
+      interfaceAction,
+      chartType
     } = this.props;
 
     return (
@@ -48,16 +66,27 @@ class Home extends Component {
             districts={districts}
             favoritesAction={favoritesAction}
             districtGeoAction={districtGeoAction}
+            demographicsAction={demographicsAction}
             selectedDistrict={selectedDistrict}/>
         </Paper>
         <div style={styles.mainContent}>
+          {selectedDistrict.length ?
+            <div style={styles.dialog}>
+              <DistrictCard
+                action={interfaceAction}
+                district={selectedDistrict[0]}
+                chartType={chartType}>
+                <PopulationChart data={selectedDemographics} chartType='bar'/>
+              </DistrictCard>
+            </div> : null}
           {districts.length ?
             <DistrictMap
               displayFavorites={displayFavorites}
               districts={districts}
               selectedDistrict={selectedDistrict}
               favorites={favorites}
-              districtGeoAction={districtGeoAction}/> : <Loader/>}
+              districtGeoAction={districtGeoAction}
+              demographicsAction={demographicsAction}/> : <Loader/>}
         </div>
       </div>
     );
@@ -66,11 +95,16 @@ class Home extends Component {
 
 Home.propTypes = {
   districts: PropTypes.array,
+  demographics: PropTypes.array,
   selectedDistrict: PropTypes.array,
+  selectedDemographics: PropTypes.object,
   favorites: PropTypes.array,
   districtGeoAction: PropTypes.object,
+  demographicsAction: PropTypes.object,
   favoritesAction: PropTypes.object,
-  displayFavorites: PropTypes.bool
+  interfaceAction: PropTypes.object,
+  displayFavorites: PropTypes.bool,
+  chartType: PropTypes.string
 }
 
 export default Home;
